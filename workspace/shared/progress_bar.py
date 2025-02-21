a3f4a77ffc7cbbcf3219c8154309ca38218b20f4
Andino Börst <andino.boerst@gmail.com>
2025-02-21 12:19:12 +0100

    Update and rename progress_bar.py to progress_bar.py

--- workspace/shared/progress_bar.py
+++ workspace/shared/progress_bar.py
@@ -6,7 +6,7 @@ def progressbar(it, prefix="", size=50, out=sys.stdout): # Python3.6+
     def show(j):
         x = int(size*j/count)
         print(f"{prefix}[{u'█'*x}{('.'*(size-x))}] {j}/{count}", end='\r', file=out, flush=True)
-    show(0.1) # avoid div/0 
+    show(0) # avoid div/0 
     for i, item in enumerate(it):
         yield item
         show(i+1)
